e00ffe536ef2d8448fca4755280b5f35548b0709
aleksey <aleksey.berezyuk@gmail.com>
2021-11-19 22:30:16 +0200

    Added auth.service

--- TestShopApp-Client/src/app/pages/login/login.component.ts
+++ TestShopApp-Client/src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { AuthResponse } from 'src/app/models/authResponse';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +9,44 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor( private toastr: ToastrService) {
-
+  creds = {
+    username: '',
+    password: ''
+  };
+ 
+  constructor(private toastr: ToastrService, private authService: AuthService) {
+  }
+  
+  getUserName(event) {
+     this.creds.username = event.target.value;
+  }
+  
+  getUserPassword(event) {
+    this.creds.password = event.target.value;
   }
 
   btnClicked() {
-    this.toastr.success('Hello world!', 'Toastr fun!', {
-      positionClass: 'toast-top-center',
-      closeButton: true
+    this.authService.authenticate(this.creds).subscribe({
+      next: data => {
+        const authResponse: AuthResponse = data;
+
+        if(authResponse.isSuccess) {
+          console.log(authResponse.token);
+          this.toastr.success('Success', '', {
+            positionClass: 'toast-top-center',
+            closeButton: true
+          });
+        } else {
+          this.toastr.warning('Invalid username or password', '', {
+            positionClass: 'toast-top-center',
+            closeButton: true
+          });
+        }
+      },
+      error: (error) => console.error(error)
     });
   }
-
+  
   ngOnInit(): void {
   }
 }
